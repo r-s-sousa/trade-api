0995bcd5740f8cba195318f8705052066df562a2
rafael <rafael_sousa2018@outlook.com>
2024-06-03 16:30:56 -0300

    Feat: Auth flow based in user and sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login');
+Route::post('/register', [AuthController::class, 'register'])
+    ->name('register');
+
+Route::post('/refresh-token', [AuthController::class, 'refreshToken'])
+    ->name('refreshToken')
+    ->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout')
+    ->middleware('auth:sanctum');
